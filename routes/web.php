a8a355f478255503ee8f829f6df6bd2dc7768c03
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2020-11-17 18:35:10 -0300

    Configuração inicial AdminLTE 3 e rota Admin

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +7,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('/admin')->namespace('Admin')->group(function() {
+    //todas as rotas Admin serão definidas aqui
+    Route::get('/', 'AdminController@index');
+
+});
+
+//Route::get('/', 'Admin\AdminController@index')->name('admin.index');
+
